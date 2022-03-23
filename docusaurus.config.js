1b8206131251c70d105907a66b4c95dcd079ca54
忘忧北萱草 <wybxc@qq.com>
2022-03-23 11:16:10 +0800

    部署 algolia 搜索

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -135,18 +135,14 @@ module.exports = {
       darkTheme: darkCodeTheme,
     },
     algolia: {
-      appId: 'YOUR_APP_ID',
+      appId: '7B4OH7MBB1',
       // Public API key: it is safe to commit it
-      apiKey: 'YOUR_SEARCH_API_KEY',
-      indexName: 'YOUR_INDEX_NAME',
-      // Optional: see doc section below
-      contextualSearch: true,
-      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-      // externalUrlRegex: 'external\\.com|domain\\.com',
-      // Optional: Algolia search parameters
-      searchParameters: {},
-      // Optional: path for search page that enabled by default (`false` to disable it)
-      searchPagePath: 'search',
+      apiKey: '446223e1842bf09e30a9f1276b07da26',
+      indexName: 'yiri-mirai',
+      contextualSearch: false,
+      searchParameters: {
+        naturalLanguages: ['zh'],
+      },
     },
   },
   presets: [
